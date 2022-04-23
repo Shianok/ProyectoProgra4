8ea732e7fe0b8a1669f8f40558e2cfb4d367aebe
Oscar <oscar.arce.ugalde@uhispano.ac.cr>
2022-04-23 08:11:21 -0600

    la wea del correo

--- Proyecto/reportes.vb
+++ Proyecto/reportes.vb
@@ -4,7 +4,6 @@ Imports System.IO
 Module reportes
     Public OWORD As New Word.Application
     Public DOCUMENTO As New Word.Document
-    Public DOCUMENTOEXCEL As New Excel.Workbook
     Public DIRECTORIO As String
     Public OTABLA As Word.Table
     Public REPORTE_DIRECCION_PDF As String = ""
